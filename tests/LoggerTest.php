b313468f664e4f613747c190b8db79229a58898d
Joseph Martineau <jamg85@msn.com>
2014-08-06 16:16:32 -0400

    LoggerTest

--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -1,12 +1,24 @@
 <?php
+error_reporting(E_ALL);
 include_once("src/Logger.php");
 include_once("src/MySqlConnection.php");
 class LoggerTest extends PHPUnit_Framework_TestCase
 {
-    public function testSample()
+    public function testLogger()
     {
-        Logger::getSingleInstace("src/log");
-        $this->fail("TODO");
+        $path = "src/log";
+        $logger = Logger::getSingleInstace($path);
+
+        $expected = "PHPUnit - Test";
+
+        $logger->write($expected);
+
+        $fd = fopen($path,'r');
+        
+
+        $actual= fread($fd, strlen($expected));
+
+        $this->assertEquals($expected, $actual);
     }
 }
 ?>
