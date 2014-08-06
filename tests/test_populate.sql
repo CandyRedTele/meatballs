5d900e2be2e3c6ad787cf143e7ce27f60d3cb82e
Joseph Martineau <jamg85@msn.com>
2014-08-06 14:35:21 -0400

    playing with sql

---
+++ tests/test_populate.sql
@@ -0,0 +1,9 @@
+use project;
+
+
+drop table staff;
+drop table local;
+
+
+insert into staff (ssn, firstname, lastname, address) values ('123456789', 'Jo', 'Martineau', '224 rue Boyer');
+insert into local (id, title) values (0, 'cook');
