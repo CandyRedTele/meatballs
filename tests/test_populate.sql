27154c1b9a145823727a3c64b061d46a933d50f8
Joseph Martineau <jamg85@msn.com>
2014-08-06 22:10:44 -0400

    removed test sql

--- tests/test_populate.sql
+++
@@ -1,9 +0,0 @@
-use project;
-
-
-drop table staff;
-drop table local;
-
-
-insert into staff (ssn, firstname, lastname, address) values ('123456789', 'Jo', 'Martineau', '224 rue Boyer');
-insert into local (id, title) values (0, 'cook');
