84fcf08858d6f2ffa27765fea4765772f2565340
Mike Hu <yipa27@hotmail.com>
2014-08-08 10:53:00 -0400

    website codes uploaded

---
+++ website/comp353Proj/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<?php session_start(); ?>
+	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+	<title> Mamma Meatballs </title>
+	<link rel="stylesheet" type="text/css" href="css/stylesheet1.css" />
+	
+	
+</head>
+	
+<body>
+<header>
+<div id="topleft">
+<nav class="menu2">
+  <menu>
+	<li></li>
+    <li><a href="welcome.php" target="insideC">Home</a></li>
+	<li><a href="#" target="insideC">Menu</a></li>
+	<li><a href="#" target="insideC">Local Delivery</a></li>
+	<li><a href="#" target="insideC">Find Us</a></li>
+    <li><a href="#" target="insideC">About Us</a></li>
+    <li><a href="#" class="selected" target="insideC">Contact Us</a></li>
+    <li></li>
+  </menu>
+</nav>
+</div>
+<div id="topright">
+	<img width="90%" height="90%" src="img/welcomeALT1.jpg"/>
+</div>
+</header>
+<iframe id="insideC" src="welcome.php">
+</body>
+	
+</html>
