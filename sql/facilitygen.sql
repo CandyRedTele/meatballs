cfbbf5adbbd0ae73defa0722cc154173abbf9593
geoffrey <geoff.the.boom@gmail.com>
2014-08-11 17:21:35 -0400

    added facility generation

---
+++ sql/facilitygen.sql
@@ -0,0 +1,2 @@
+insert into facility (f_id, location, m_id, phone) values (1, "montreal", 2, "3333");
+insert into facilityStock (quantity, order_date, sku, f_id) values (10, current_date, 25173, 1);
--- sql/populate.sql
+++ sql/populate.sql
@@ -4,3 +4,4 @@ source create_table.sql;
 source staff/pay.sql;
 source supply_menu/supply_menu.sql;
 source staff/staffgen.sql;
+source facility/gen_facility.sql;
