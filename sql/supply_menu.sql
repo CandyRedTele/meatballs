50e71734236336e25714d74cb0a7c47251f4a298
Samuel Masuy <samuel.masuy@gmail.com>
2014-08-10 21:54:15 -0400

    cleaned a bit the folder

--- sql/supply_menu.sql
+++
@@ -1,9 +0,0 @@
-use meatballs;
-
-source supply_menu/menu_item.sql;
-source supply_menu/supply.sql;
-source supply_menu/other_supply.sql;
-source supply_menu/ingredients.sql;
-source supply_menu/menu.sql;
-source supply_menu/wine.sql;
-source supply_menu/food.sql;
---
+++ sql/supply_menu/supply_menu.sql
@@ -0,0 +1,9 @@
+use meatballs;
+
+source menu_item.sql;
+source supply.sql;
+source other_supply.sql;
+source ingredients.sql;
+source menu.sql;
+source wine.sql;
+source food.sql;
