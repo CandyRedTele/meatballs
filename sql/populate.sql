eb383f9b6139f381f4c56462f467861a127e48f6
geoffrey <geoff.the.boom@gmail.com>
2014-08-13 04:10:19 -0400

    added bill_has_items generator. it works, but the menu_item isn't necessarily from the facility that the bill is for.

--- sql/populate.sql
+++ sql/populate.sql
@@ -7,9 +7,9 @@ source supply_menu/supply_menu.sql;
 source facility/gen_facility.sql;
 
 source staff/gen_localStaff.sql;
-source bills/gen_bills.sql;
 
 source facility/gen_facilityHours.sql;
 source supply_menu/facility_stock.sql;      # this one needs the facility table to be created before
 
-
+source bills/gen_bills.sql;
+source bills/gen_bill_has_items.sql;
