70dda5885142f1a8d9dda64cd1f4627113d00c02
geoffrey <geoff.the.boom@gmail.com>
2014-08-13 10:16:59 -0400

    added script to count golden_bills

--- sql/scripts/ingredients_per_menu.sql
+++ sql/scripts/ingredients_per_menu.sql
@@ -1,2 +1,1 @@
-
 select m_id, mitem_id, menu_item.name as 'item name', supplies.sku, supplies.name as 'supply name'   from menu natural join menu_item natural join ingredients, supplies where menu.m_id = 1 and supplies.sku = ingredients.sku;
