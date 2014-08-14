8e1b8191775b27037ff2e5d5fec31a79e48030e4
geoffrey <geoff.the.boom@gmail.com>
2014-08-14 16:57:40 -0400

    starting work on the shift schedule

--- sql/staff/gen_admin.sql
+++ sql/staff/gen_admin.sql
@@ -3,4 +3,7 @@ insert into admin ( staff_id,  location,  yrs_exp) Values
 (1, 'Toronto', 1),
 (2, 'Winipeg', 2),
 (3, 'Narnia', 3),
-(9, 'Regina', 4);
+(30, 'Halifax', 0),
+(66, 'Halifax', 1),
+(120, 'Montreal', 0),
+(150, 'Halifax', 0);
