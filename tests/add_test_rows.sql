8361bcbc1b7463b4102139096b9b963c6f159538
Joseph Martineau <jamg85@msn.com>
2014-08-13 22:12:20 -0400

    [TESTS] just notes on the tests I performed...

--- tests/add_test_rows.sql
+++ tests/add_test_rows.sql
@@ -1,2 +1,9 @@
-use meatballs;
 insert into localstaff (f_id, staff_id) values (1, 10);
+
+
+#
+# testing trigger : update_stock_trigger
+#
+select * from facilityStock where sku=99689;
+insert into `order` (f_id, sku, order_qty) values (1, 99689, 666);
+select * from update_stock_log;
