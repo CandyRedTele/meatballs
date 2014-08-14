a6f2f77f2c7c624f33ecdc4028fe49d74b009a9e
geoffrey <geoff.the.boom@gmail.com>
2014-08-14 17:01:53 -0400

    tested tables, added ShiftSet.java

--- sql/staff/gen_admin.sql
+++ sql/staff/gen_admin.sql
@@ -2,8 +2,4 @@ use meatballs;
 insert into admin ( staff_id,  location,  yrs_exp) Values
 (1, 'Toronto', 1),
 (2, 'Winipeg', 2),
-(3, 'Narnia', 3),
-(30, 'Halifax', 0),
-(66, 'Halifax', 1),
-(120, 'Montreal', 0),
-(150, 'Halifax', 0);
+(3, 'Narnia', 3);
