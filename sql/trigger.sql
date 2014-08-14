6db984d32c27665666639c7ff5a48bb6ca9c83d3
Joseph Martineau <jamg85@msn.com>
2014-08-14 12:13:00 -0400

    [SQL] TRIGGER -> to update facilityBalance when INSERT on

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -22,6 +22,26 @@ CREATE TABLE IF NOT EXISTS update_stock_log
 
 -- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 -- 
+-- TABLE : update_balance_log
+-- 
+-- PURPOSE : Log the updates made to the facilityStock table
+--
+-- ---------------------------------------------------------
+DROP TABLE IF EXISTS update_balance_log;
+CREATE TABLE IF NOT EXISTS update_balance_log
+(
+    log_id      INTEGER PRIMARY KEY AUTO_INCREMENT,
+    msg         VARCHAR(255),
+    f_id        INTEGER,
+    new_balance     INTEGER,
+    old_balance     INTEGER,
+    FOREIGN KEY (`f_id`) REFERENCES `facilityBalance` (`f_id`)
+        ON DELETE NO ACTION
+        ON UPDATE CASCADE
+);
+
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- 
 -- TRIGGER : update_stock_trigger 
 -- 
 -- PURPOSE : update the facilityStock quantity when a row
@@ -34,38 +54,31 @@ CREATE TRIGGER update_stock_trigger
 AFTER INSERT ON `order`
 FOR EACH ROW
 BEGIN
+    -- 1. update `facilityStock`
     SET @old_qty = (SELECT quantity FROM facilityStock WHERE sku = NEW.sku);
+
     INSERT INTO facilityStock (sku, f_id, quantity) 
         VALUES (NEW.sku, NEW.f_id, NEW.order_qty)
         ON DUPLICATE KEY UPDATE quantity = @old_qty + NEW.order_qty;
 
+    -- 1.1 Log it in `update_stock_log`
     SET @new_qty = (SELECT quantity FROM facilityStock WHERE sku = NEW.sku);
-
     INSERT INTO update_stock_log (msg, sku, qty_old, qty_new)
         VALUES ("Update Stock after order", NEW.sku, @old_qty, @new_qty); 
+
+    -- 2. update `facilityBalance`
+    SET @old_balance = (SELECT balance FROM facilityBalance WHERE f_id = NEW.f_id);
+    SET @price      = (SELECT price from supplies WHERE sku = NEW.sku);
+
+    INSERT INTO facilityBalance (f_id, balance)
+        VALUES (NEW.f_id, @price * NEW.order_qty)
+        ON DUPLICATE KEY UPDATE balance = @old_balance + (@price * NEW.order_qty);
+
+    -- 2.1 Log it in `update_balance_log`
+    SET @new_balance = (SELECT balance FROM facilityBalance WHERE f_id = NEW.f_id);
+    INSERT INTO update_balance_log (msg, f_id, old_balance, new_balance)
+        VALUES ("Update balance after order", NEW.f_id, @old_qty, @new_qty); 
+
 END; $$$
 DELIMITER ;
 
-# Attempt to create a trigger to remove money from the facility's balance but
-# MySql do not support more then one trigger of the same type on the same table.
-
--- DROP TRIGGER IF EXISTS update_balance_on_order_trigger;
--- DELIMITER $$$
--- CREATE TRIGGER update_balance_on_order_trigger
--- AFTER INSERT ON `order`
--- FOR EACH ROW
--- BEGIN
---     SET @old_balance = (SELECT balance FROM facilityBalance WHERE f_id = NEW.f_id);
--- 
---     SET @price = (SELECT supplies.price FROM supplies WHERE supplies.sku = NEW.sku);
--- 	
---     UPDATE facilityBalance
---     SET balance = @old_balance - @price
---     WHERE f_id = NEW.f_id;
--- 
---     SET @new_balance = (SELECT balance FROM facilityBalance WHERE f_id = NEW.f_id);
--- 
---     INSERT INTO update_balance_log (msg, f_id, balance_old, balance_new)
---         VALUES ("Update Balance after order", NEW.f_id, @old_balance, @new_balance); 
--- END; $$$
--- DELIMITER ;
