7f233179ca358c52725652ec9f920e61ecc77cb2
Samuel Masuy <samuel.masuy@gmail.com>
2014-08-14 00:10:54 -0400

    Attempt to create a second trigger on order: FAIL

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -45,3 +45,27 @@ BEGIN
         VALUES ("Update Stock after order", NEW.sku, @old_qty, @new_qty); 
 END; $$$
 DELIMITER ;
+
+# Attempt to create a trigger to remove money from the facility's balance but
+# MySql do not support more then one trigger of the same type on the same table.
+
+-- DROP TRIGGER IF EXISTS update_balance_on_order_trigger;
+-- DELIMITER $$$
+-- CREATE TRIGGER update_balance_on_order_trigger
+-- AFTER INSERT ON `order`
+-- FOR EACH ROW
+-- BEGIN
+--     SET @old_balance = (SELECT balance FROM facilityBalance WHERE f_id = NEW.f_id);
+-- 
+--     SET @price = (SELECT supplies.price FROM supplies WHERE supplies.sku = NEW.sku);
+-- 	
+--     UPDATE facilityBalance
+--     SET balance = @old_balance - @price
+--     WHERE f_id = NEW.f_id;
+-- 
+--     SET @new_balance = (SELECT balance FROM facilityBalance WHERE f_id = NEW.f_id);
+-- 
+--     INSERT INTO update_balance_log (msg, f_id, balance_old, balance_new)
+--         VALUES ("Update Balance after order", NEW.f_id, @old_balance, @new_balance); 
+-- END; $$$
+-- DELIMITER ;
