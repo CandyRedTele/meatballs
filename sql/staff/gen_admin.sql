8d05c043e94023bb46c6190eec4e0d20f9452435
geoffrey <geoff.the.boom@gmail.com>
2014-08-15 14:26:05 -0400

    moved java files to dataGeneration folder

--- sql/staff/gen_admin.sql
+++ sql/staff/gen_admin.sql
@@ -1,5 +1,5 @@
 use meatballs;
 insert into admin ( staff_id,  location,  yrs_exp) Values
-(1, 'Quebec', 2),
-(2, 'Halifax', 1),
-(3, 'Ottowa', 3);
+(1, 'Toronto', 1),
+(2, 'Sherbrooke', 3),
+(3, 'Vancouver', 2);
