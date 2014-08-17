f3553b473311a72cbc38f0b0afdb3a523b46d778
Samuel Masuy <samuel.masuy@gmail.com>
2014-08-16 23:46:11 -0400

    Menu display

--- website/menu/menuWINNIPEG.php
+++ website/menu/menuWINNIPEG.php
@@ -1,7 +1,8 @@
 <?php 
 	error_reporting(E_ALL);
-	set_include_path($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/src');
-        include_once("IncludeAllQueries.php");
+	//set_include_path($_SERVER['DOCUMENT_ROOT'] . '/comp353-project/src');
+ include_once("../../src/SetPath.php");
+include_once("IncludeAllQueries.php");
 	session_start();
 ?>
 <!DOCTYPE html>
@@ -18,24 +19,19 @@
 $(function() {
 	// grab the initial top offset of the navigation 
 	var sticky_navigation_offset_top = $('#sticky_navigation').offset().top;
-	
 	// our function that decides weather the navigation bar should have "fixed" css position or not.
 	var sticky_navigation = function(){
 		var scroll_top = $(window).scrollTop(); // our current vertical position from the top
-		
 		// if we've scrolled more than the navigation, change its position to fixed to stick to top, otherwise change it back to relative
 		if (scroll_top > sticky_navigation_offset_top)
 			$('#sticky_navigation').css({ 'position': 'fixed', 'top':0, 'left':0 });
 		else 
 			$('#sticky_navigation').css({ 'position': 'relative' }); 
 	};
-	
 	// run our function on load
 	sticky_navigation();
-	
 	// and run it again every time you scroll
 	$(window).scroll(function() { sticky_navigation();});
-	
 	// NOT required:
 	// for this demo disable all links that point to "#"
 	$('a[href="#"]').click(function(event){	event.preventDefault(); });
@@ -45,60 +41,35 @@ $(function() {
 <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<?php include_once("menuNAVIGATION.php"); ?>
-<!--								MENU starts HERE ! ! !							-->
+<?php
+$parameter = $_SERVER['QUERY_STRING'];
+echo "<div id='main'>
+		<div class='width-container'>
+			<div class='grid2column'>";
 
-	<div id="main">
-		<div class="width-container">
-			<div class="grid2column">
-				<h3 class="header-underline">Main Courses</h3>
-				<ul class="menu-items">
-					<li>
-						<a href="menu-post.html"><div class="grid2column">Roasted Apple Tart</div>
-						<div class="grid2column lastcolumn">$12,00</div>
-						<div class="clearfix"></div>
-						<div class="item-description-menu">Cras eget est tempor odio molestie ultricies. Donec gravida libero at metus tincidunt sit amet.</div></a>
-					</li>
-					
-				</ul>
-				
-				<div class="menu-spacer"></div>
-			</div>
-			<div class="grid2column lastcolumn">
-				<h3 class="header-underline">Drinks</h3>
-				<ul class="menu-items">
-					<li>
-						<div class="grid2column">Cola</div>
-						<div class="grid2column lastcolumn">$4,00</div>
-						<div class="clearfix"></div>
-					</li>
-					
-				</ul>
-				
-				<div class="menu-spacer"></div>
-				<h3 class="header-underline">Salads</h3>
-				<ul class="menu-items">
-					<li>
-						<div class="grid2column">Sundried Tomato Salad</div>
-						<div class="grid2column lastcolumn">$10,00</div>
-						<div class="clearfix"></div>
-						<div class="item-description-menu">Cras eget est tempor odio molestie ultricies. Donec gravida libero at metus tincidunt sit amet.</div>
-					</li>
-					
-				</ul>
-				
-				<div class="menu-spacer"></div>
-			</div>
-			<div class="clearfix"></div>
-			
-			
-		<div class="clearfix"></div>
-		</div><!-- 						close .width-container (INNER MENU ends HERE!!!)							-->
-		
-	</div><!-- close #main -->
-	
-<!--								MENU ends HERE ! ! !							-->
+$query1 = new CustomQuery("select distinct category from menu_item");
+//$query = new SelectAllQuery("customers");
+if (!is_null($query1)) { $categories = $query1->execute();}
 
-
+while($category = mysqli_fetch_row($categories)) {
+    echo "<h3 class='header-underline'>".$category[0]."</h3>";
+    $query2 = new CustomQuery("select name, price from menu_item natural join (menu natural join facility) where
+        facility.f_id ='" . $parameter . "' and menu_item.category = '". $category[0]. "'");
+    if (!is_null($query2)) { $menus_items = $query2->execute();}
+    echo "<ul class='menu-items'>";
+    while($menu_items = mysqli_fetch_row($menus_items)) {
+        echo "<li>";
+        //foreach ($menu_items as $menu_item) {
+            echo "<a href='#'><div class='grid2column'>" . $menu_items[0] . "</div></a>
+            <a href='#'><div class='grid2column lastcolumn'>" . $menu_items[1] . "</div></a>
+            <div class='clearfix'></div>";
+        //}
+        echo "</li>";
+    }
+    echo "</ul>";
+    echo "<div class='menu-spacer'></div>";
+}
+echo "</div></div></div>";
+?>
 </body>
 </html>
