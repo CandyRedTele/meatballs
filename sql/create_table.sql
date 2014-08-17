08d0a4aab3aedfe555762df0f1fc1576feb9ff7c
Joseph Martineau <jamg85@msn.com>
2014-08-17 14:14:57 -0400

    [SQL] added default value to yrs_exp

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -79,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `meatballs`.`admin`
 (
     `staff_id`  INTEGER NOT NULL,
     `location`  VARCHAR(55) NULL DEFAULT 'Montreal',
-    `yrs_exp`   INTEGER NULL,
+    `yrs_exp`   INTEGER     NOT NULL DEFAULT 0,
     `training`  VARCHAR(45) NULL,
     PRIMARY KEY (staff_id, location), -- Assuming that an employee might work in different locations during his career.
     CONSTRAINT `fk_admin_staff_id`
