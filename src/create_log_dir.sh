a9ee22f8223e3f29d5241d97c21e557483c2c728
Joseph Martineau <jamg85@msn.com>
2014-08-19 12:02:40 -0400

    [BASH] fix Darwin

--- src/create_log_dir.sh
+++ src/create_log_dir.sh
@@ -7,7 +7,7 @@ mkdir -p log
 #
 # add /log to group SYSTEM on Windows and daemon on Linux/Max and give WRITE permission to group
 #
-if [  $(uname -o) == 'Cygwin' ]; then
+if [ $(uname) != 'Darwin' -a $(uname -o) == 'Cygwin' ]; then
     # Windows user that runs scripts from Cygwin
     chown $USER:SYSTEM log
 else 
