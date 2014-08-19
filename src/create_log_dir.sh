74fd0f39bd43356fc8620b519675117fa6fc7eb7
Joseph Martineau <jamg85@msn.com>
2014-08-18 21:59:56 -0400

    [BASH] added comments

--- src/create_log_dir.sh
+++ src/create_log_dir.sh
@@ -8,8 +8,10 @@ mkdir -p log
 # add /log to group SYSTEM on Windows and daemon on Linux/Max and give WRITE permission to group
 #
 if [  $(uname -o) == 'Cygwin' ]; then
+    # Windows user that runs scripts from Cygwin
     chown $USER:SYSTEM log
 else 
+    # Linux, Unix and Mac users
     sudo chown $USER:daemon log
 fi
 
