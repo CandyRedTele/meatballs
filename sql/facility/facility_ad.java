47436bbc74c41a6acf0f123b21661031d5107944
Samuel Masuy <samuel.masuy@gmail.com>
2014-08-19 16:57:14 -0400

    added 12 facility address to be mapped with the facility table

---
+++ sql/facility/facility_ad.java
@@ -0,0 +1,16 @@
+ String[] f_address =
+                {
+                 "1455 Boulevard de Maisonneuve Ouest Montreal, QC H3G 1M8",
+                 "27 King's College Cir Toronto, ON M5S",
+                 "66 Chancellors Cir Winnipeg, MB R3T 2N2",
+                 "500 College Dr Narnia, YT Y1A 5K4",
+                 "2500 University Dr NW Calgary, AB T2N 1N4",
+                 "1111 Resolute, Faraway, NU X0A 0V0",
+                 "6299 South St Halifax, NS B3H 4R2",
+                 "75 Laurier Ave E Ottawa, ON K1N 6N5",
+                 "2329 W Mall Vancouver, BC V6T 1Z4",
+                 "3737 Wascana Pkwy Regina, SK S4S 0A2",
+                 "2325 Rue de l'Universite Quebec, QC G1V 0A6",
+                 "2500 boul. de l'Universite Sherbrooke, QC J1K 2R1",
+                };
+
