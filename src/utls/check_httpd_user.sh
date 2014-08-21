49ff98f8dba5eeb8f4b92c78ed99045131d8bfc8
Joseph Martineau <jamg85@msn.com>
2014-08-21 06:32:01 -0400

    [sh] httpd find

--- src/utls/check_httpd_user.sh
+++ src/utls/check_httpd_user.sh
@@ -1,5 +1,8 @@
 #
 # check which user is running HTTPD
 #
-HTTPD_FILE=$(find / -name httpd.conf  2>/dev/null)
-egrep -iw --color=auto '^user|^group' $HTTPD_FILE
+
+
+
+find /opt -name httpd.conf  2>/dev/null | xargs egrep -iw --color=auto '^user|^group'
+
