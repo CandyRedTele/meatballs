20b2386fe0e454d90c868bf407c46e7e45e5850f
Agoudabou <genevieveouellon@geneviellonsmbp.wireless.concordia.ca>
2014-08-24 16:13:45 -0400

    [unittest] IQuery_2Test.php

--- tests/unit/IQuery_2Test.php
+++ tests/unit/IQuery_2Test.php
@@ -24,140 +24,33 @@ class IQuery_2Test extends PHPUnit_Framework_TestCase
        
 
         // 1. Create a new instance of GetAccessLevelQuery based on staff id.
-        $access = new GetAccessLevelQuery($staff_id);
-        $result = $access->execute();
+        $access_query = new GetAccessLevelQuery($staff_id);
+        $result = $access_query->execute();
 
-            if(!$result) {
-                $this->fail('execute failed');
+        if(!$result) {
+            $this->fail('execute failed');
         }
 
-        // 2. Retreive the access level value.
-        $row = mysqli_fetch_row($result);
-        $access = $row[0];
-/*
-        if (!$result) {
-            mysqli_free_result($result);
+        // 2. Retreive the expected access level.
+        $row = mysqli_fetch_assoc($result);
+        $access = $row['access_level'];
+
+        // 3. Get a value to compare.
+        $select = new CustomQuery("select access_level from access_level natural join staff where staff_id='".$staff_id."';");
+        $result1 = $select->execute();
+
+        if (!$result1) {
+            mysqli_free_result($result1);
             $this->assertTrue(false); // Fail it.
         }
-*/
-        $actual = mysqli_fetch_assoc($result);
-        mysqli_free_result($result);
 
-        // 3. assert
-        
+        $actual = mysqli_fetch_assoc($result1);
+        mysqli_free_result($result1);
+
+        // 4. assert
         $this->assertEquals($access, $actual['access_level']);
-        
-        //$this->fail("TODO");
-
-    }
-/*
-    public function testInsertIntoStaffQuery_localstaffGetsUpdated()
-    {
-        $name = 'Jean-Paul III';
-        $title = 'cook';
-        $f_id = 1;
-
-        // 1. Insert some values.
-        $insert= new InsertIntoStaffQuery($name, 
-                                            '224 rue XYZ', 
-                                            '819-789-1234', 
-                                            '123456789', 
-                                            $title, 
-                                            $f_id);
-        if(!$insert->execute()) {
-            $this->fail('execute failed');
-        }
-
-        
-        // 2. Retreive those values.
-        $select = new CustomQuery("select name, title from localstaff natural join staff where name='".$name."';");
-        $result = $select->execute();
-        
-        if (!$result) {
-            mysqli_free_result($result);
-            $this->assertTrue(false); // Fail it.
-        }
-
-
-        $actual = mysqli_fetch_assoc($result);
-        mysqli_free_result($result);
-
-        // 3. assert
-        $this->assertEquals($name, $actual['name']);
-        $this->assertEquals($title, $actual['title']);
 
     }
 
-
-    public function testInsertIntoStaffQuery_adminGetsUpdated()
-    {
-        $name = 'Jean-Paul II';
-        $title = 'ceo';
-
-        // 1. Insert some values.
-        $insert= new InsertIntoStaffQuery($name, 
-                                            '224 rue XYZ', 
-                                            '819-789-1234', 
-                                            '123456789', 
-                                            $title, 
-                                            'Montreal');
-
-        if(!$insert->execute()) {
-            $this->fail('execute failed');
-        }
-
-        
-        // 2. Retreive those values.
-        $select = new CustomQuery("select name, title from admin natural join staff where name='".$name."';");
-        $result = $select->execute();
-        
-        if (!$result) {
-            mysqli_free_result($result);
-            $this->assertTrue(false); // Fail it.
-        }
-
-
-        $actual = mysqli_fetch_assoc($result);
-        mysqli_free_result($result);
-
-        // 3. assert
-        $this->assertEquals($name, $actual['name']);
-        $this->assertEquals($title, $actual['title']);
-
-    }
-
-
-    public function testCustomQuery()
-    {
-        $expected = 'phpunit';
-
-        // 1. Insert some values.
-        $insert = new CustomQuery("insert into staff (name) values ('" . $expected . "');");
-
-        if (!($result = $insert->execute())) {
-            $this->fail('insertion failed');    
-        }
-        
-        // 2. Retreive those values.
-        $select = new CustomQuery("select name from staff where name='phpunit';");
-        $result = $select->execute();
-        
-        if (!$result) {
-            $this->fail('result is null');
-        }
-
-        $actual = mysqli_fetch_row($result);
-
-        // 3. assert
-        $this->assertEquals($expected, $actual[0]);
-
-        mysqli_free_result($result);
-    }
-
-    public function testGetAccessLevelQuery()
-    {
-        $this->markTestSkipped('TODO');
-    }
-    //*/
 }
 ?>
