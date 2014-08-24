39dc190b1895b62a5a07e8a8a40cab617683be66
geoffrey <geoff.the.boom@gmail.com>
2014-08-24 15:55:22 -0400

    removed repeats of training

--- dataGeneration/generate_data.java
+++ dataGeneration/generate_data.java
@@ -100,18 +100,20 @@ public class generate_data {
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		//ask for current date
-		System.out.println("when was the last monday(int)?");
 		Scanner gimmedate = new Scanner(System.in);
-		int lastMonday = gimmedate.nextInt();
+		
+		System.out.println("what is the current date(int)?");
+		int currDate = gimmedate.nextInt();
 		
 		System.out.println("what is the current month(int)?");
 		int currMonth = gimmedate.nextInt();
 		
-		System.out.println("WHAT YEAR IS IT(int");
+		System.out.println("what is the current year(int)?");
 		int currYear = gimmedate.nextInt();
 		
-		System.out.println("what is the current date(int)?");
-		int currDate = gimmedate.nextInt();
+		System.out.println("when was the last monday(int)?");
+		int lastMonday = gimmedate.nextInt();
+
 		
 		gimmedate.close();
 		
@@ -394,16 +396,7 @@ public class generate_data {
 			if(!isAdmin(staff.title)){
 				
 				if(staff.title.equals(titles[ChefId])){
-					
-					int numTrain = random_num(1,4);
-					String training = "";
-					
-					for(int jay = 0; jay < numTrain; jay++){
-						if(jay != numTrain-1)
-							training += cookTraining[random_num(0, cookTraining.length-1)] + ", ";
-						else
-							training += cookTraining[random_num(0, cookTraining.length-1)];
-					}
+					String training = gen_training(cookTraining,4);
 					staff.addTraining(training);
 				}
 				else
@@ -458,15 +451,7 @@ public class generate_data {
 			StaffMember staff = arrStaff[i];
 			if(isAdmin(staff.title)){
 
-				int numTrain = random_num(1,4);
-				String training = "";
-				
-				for(int jay = 0; jay < numTrain; jay++){
-					if(jay != numTrain-1)
-						training += admin_training[random_num(0, admin_training.length-1)] + ", ";
-					else
-						training += admin_training[random_num(0, admin_training.length-1)];
-				}
+				String training = gen_training(admin_training, 4);
 				
 				staff.location = "Montreal";
 				staff.yrs_exp = random_num(0, 4);
@@ -481,6 +466,7 @@ public class generate_data {
 			}
 			
 		}
+		
 		/*
 		for(int i = 1; staff_reader.hasNext(); i++){
 			String nextLine = staff_reader.nextLine();
@@ -504,6 +490,20 @@ public class generate_data {
 		
 		if(debug) System.out.println("Admins Generated");
 
+	}
+	
+	private static String gen_training(String[] trainingTypes, int maxTrain){
+		int[] seq = random_sequence(sequence(trainingTypes.length-1, 1));
+		int numTrain = random_num(1, maxTrain);
+		String training = "";
+		for(int jay = 0; jay < numTrain; jay++){
+			if(jay != numTrain-1)
+				training += trainingTypes[seq[jay]] + ", ";
+			else
+				training += trainingTypes[seq[jay]];
+		}
+		
+		return training;
 	}
 	
 	static boolean isAdmin(String title){
