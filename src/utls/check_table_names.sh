f428346ee9c05a2d998baff03738948b8b761fa0
Joseph Martineau <jamg85@msn.com>
2014-08-23 23:04:20 -0400

    [SH] header

--- src/utls/check_table_names.sh
+++ src/utls/check_table_names.sh
@@ -1,6 +1,18 @@
-
+#******************************************************************************
+#
+# AUTHOR : Joseph, comp353-project TEAM 3
+#
+# PURPOSE : do a global search and replace on frequently mistype 
+#           table names/methods
+# 
+# NOTE : tested on BASH
+#
+#******************************************************************************
 
 declare -A TABLE_NAMES
+#
+# Add some more in the array, if needed
+#
 TABLE_NAMES=([facilitystock]='facilityStock' 
              [facilitybalance]='facilityBalance'
              [facilityhours]='facilityHours'
